ca26ad82a9cc9cabceef041da34125711e81752d
evancastillo <80366964+evancastillo@users.noreply.github.com>
2021-04-11 17:00:13 -0700

    added project code

---
+++ script.js
@@ -0,0 +1,25 @@
+// Assignment code here
+function clickButton() {
+  var uppers = confirm("Would you like to use uppercase letters?");
+  var lowers = confirm("Would you like to use lowercase letters?");
+  var numbers = confirm("Would you like to use numbers?");
+  var symbols = confirm("Would you like to use special characters?");
+  var length = prompt("How many characters would you like your password to be?");
+  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+}
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
